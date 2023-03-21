350f30c7372dfe92fd184979a04a8b7e7be7cd26
GenerelSchwerz <xXSG.KingXx@gmail.com>
2023-03-21 15:22:20 -0400

    update

--- src/modules/chatBot.ts
+++ src/modules/chatBot.ts
@@ -22,7 +22,7 @@ export class ChatBotModule extends AFKModule<ChatBotModuleOptions> {
         super.perform();
         const max = this.options.count ?? this.options.messages.length;
         let i = 0;
-        while (i < this.options.messages.length && i < max && !this.shouldCancel) {
+        while (i < max && !this.shouldCancel) {
             this.bot.chat(this.options.messages[this.options.random ? Math.floor(this.options.messages.length * Math.random()) : i]);
             await sleep(this.options.delay + (this.options.variation * (Math.random() > 0.5 ? -Math.random() : Math.random())))
             i++;
