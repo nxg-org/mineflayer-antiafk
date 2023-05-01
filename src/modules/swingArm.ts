304df9db0f804b9cda8ae4c2f1d59c474b3834ff
GenerelSchwerz <xXSG.KingXx@gmail.com>
2023-04-30 22:04:04 -0400

    make swing arm swing arm

--- src/modules/swingArm.ts
+++ src/modules/swingArm.ts
@@ -3,7 +3,7 @@ import { AFKModule } from './module'
 export class SwingArm extends AFKModule {
   public override async perform (): Promise<boolean> {
     super.perform()
-    this.bot.swingArm('right')
+    this.bot.swingArm('right', true)
     return this.complete(true)
   }
 }
